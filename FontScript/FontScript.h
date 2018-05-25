99f7164567ca3d821329b9a2989bf77f2c540260
David Schweinsberg <david.schweinsberg@gmail.com>
2018-05-25 15:58:33 -0700

    Initial Commit

---
+++ FontScript/FontScript.h
@@ -0,0 +1,19 @@
+//
+//  FontScript.h
+//  FontScript
+//
+//  Created by David Schweinsberg on 5/25/18.
+//  Copyright © 2018 David Schweinsberg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FontScript.
+FOUNDATION_EXPORT double FontScriptVersionNumber;
+
+//! Project version string for FontScript.
+FOUNDATION_EXPORT const unsigned char FontScriptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontScript/PublicHeader.h>
+
+
